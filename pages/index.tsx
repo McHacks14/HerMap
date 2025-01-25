fd341d2e0caffa51a66cc7e577c32cebf529f14a
Janelle Tam <janelle.tam@mail.mcgill.ca>
2025-01-25 15:03:28 -0500

    Add basic map again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import Head from 'next/head';
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 45.4211,
+    longitude: -75.6903,
+    zoom: 10
+  });
+
   return (
-    <div>mappp
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      >
+        <NavigationControl position="top-left" />
+      </ReactMapGL>
     </div>
   );
 }
-//
