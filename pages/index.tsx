af286fdf07099e7b5c1803506d701cfa0417080c
Janelle Tam <janelle.tam@mail.mcgill.ca>
2025-01-26 10:37:57 -0500

    More resolve merge conflicts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import Head from 'next/head';
-import React, { useState } from 'react';
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-export default function Home() {
-  const [viewport, setViewport] = useState({
-    width: '100vw',
-    height: '100vh',
-    latitude: 45.4211,
-    longitude: -75.6903,
-    zoom: 10
-  });
-
-  return (
-    <div>
-      mapp
-      <ReactMapGL
-        {...viewport}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-        onViewportChange={(nextViewport: React.SetStateAction<{ width: string; height: string; latitude: number; longitude: number; zoom: number; }>) => setViewport(nextViewport)}
-      >
-        <NavigationControl style={{ position: 'absolute', top: 10, left: 10 }} />
-      </ReactMapGL>
-    </div>
-  );
-}
-=======
 import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -247,4 +217,3 @@ const MapComponent: React.FC = () => {
 };
 
 export default MapComponent;
->>>>>>> 58920a85419af96fc6e23c669d206c690e10a633
